a5e6cd044e8ef8fe9d876793dc5075f8ac5a37da
Stijn Deknudt <stijn@ebisi.be>
2026-03-13 08:19:39 +0100

    feat: Initial commit

---
+++ src/main/java/be/coderdojo/ninove/coderdojo/CoderdojoApplication.java
@@ -0,0 +1,13 @@
+package be.coderdojo.ninove.coderdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderdojoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoderdojoApplication.class, args);
+  }
+
+}
